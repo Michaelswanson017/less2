8d643b95631749061e6aee82bb95f5b69eb720c7
Dimas <michaelswanson017@gmail.com>
2019-10-09 19:10:35 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let arr = ['plum', 'top'];
+
+console.log(arr[1]);
+
+
+// let answer = confirm("Are you here&");
+// console.log(answer);
+
+
+let answer = prompt("Есть ли вам 18?", "ДА");
+// console.log(answer);
+
+console.log(typeof(answer));
